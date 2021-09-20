ac06efada202b9509a50ec47c7684bf9affe7a54
ajaykatkade <akatkade@LIN51014266.corp.capgemini.com>
2021-09-20 11:23:01 +0530

    Changes in BootClass

--- sampleapp/src/main/java/org/capg/demo/BootClass.java
+++ sampleapp/src/main/java/org/capg/demo/BootClass.java
@@ -4,6 +4,10 @@ public class BootClass {
  public static void main(String[] args) {
 	System.out.println("Welcome to Maven Java App");
 	System.out.println("This is running");
-
+	int arr[]=new int[200];
+	for(int i=0;i<200;i++) {
+		arr[i]=i;
+	}
+	
 }
 }
