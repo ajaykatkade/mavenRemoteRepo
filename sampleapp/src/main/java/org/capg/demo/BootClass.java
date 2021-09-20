4760844b494221f73fa0836b78102e5f0da305c0
ajaykatkade <akatkade@LIN51014266.corp.capgemini.com>
2021-09-20 11:14:47 +0530

    # WARNING: head commit changed in the meantime
    Merge branch 'reportFeature'

--- sampleapp/src/main/java/org/capg/demo/BootClass.java
+++ sampleapp/src/main/java/org/capg/demo/BootClass.java
@@ -4,6 +4,10 @@ public class BootClass {
  public static void main(String[] args) {
 	System.out.println("Welcome to Maven Java App");
 	System.out.println("This is running");
+	
+	for(int i=0;i<1000;i++) {
+		System.out.println(i);
+	}
 
 }
 }
